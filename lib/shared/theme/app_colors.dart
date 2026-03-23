b79845eddca491b22063f3da86392a926d289754
ricarthlima <ricarth.lima@gmail.com>
2026-03-23 11:46:38 -0300

    chore: adicionado arquivo de cores

---
+++ lib/shared/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color background = Color(0xFF6B802F);
+  static const Color darkBackground = Color(0xFF29330E);
+  static const Color border = Color.fromARGB(136, 54, 64, 24);
+  static const Color glow = Color(0xFFC1E655);
+
+  static const Color main = Color.fromARGB(255, 218, 163, 0);
+}
