f28fa654b73ee4461a59b727a9d9ff8ffc74127e
ricarthlima <ricarth.lima@gmail.com>
2026-03-24 11:10:46 -0300

    test: configurada suite de testes

--- lib/shared/theme/app_colors.dart
+++ lib/shared/theme/app_colors.dart
@@ -5,6 +5,5 @@ abstract class AppColors {
   static const Color darkBackground = Color(0xFF29330E);
   static const Color border = Color.fromARGB(136, 54, 64, 24);
   static const Color glow = Color(0xFFC1E655);
-
   static const Color main = Color.fromARGB(255, 218, 163, 0);
 }
